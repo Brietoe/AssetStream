193219302c6b6900c1052ab769c8391f20603577
Brietoe <Brietoe@gmail.com>
2021-04-16 18:14:59 -0500

    Separate Test message

--- Source/AssetStream.test.ts
+++ Source/AssetStream.test.ts
@@ -2,7 +2,9 @@ import AssetStream from "./AssetStream.js";
 import Testing from "../../Testing/Release/Testing.js";
 import { isDefined } from "../../Testing/node_modules/@cc-tech/library/Release/cc-tech.js";
 const assetStream = Testing(AssetStream);
-assetStream({ command: "Add" }).claim((result) =>
+
+const testCommand = { command: "Add" };
+assetStream(testCommand).claim((result) =>
 {
 	return isDefined(result.request);
 });
