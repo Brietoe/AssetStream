4e71ab1578e55d65557cf3ce821b402913e00ead
Brietoe <Brietoe@gmail.com>
2021-04-16 18:04:07 -0500

    Add claim to initial test

--- Source/AssetStream.test.ts
+++ Source/AssetStream.test.ts
@@ -1,4 +1,8 @@
 import AssetStream from "./AssetStream.js";
 import Testing from "../../Testing/Release/Testing.js";
+import { isDefined } from "../../Testing/node_modules/@cc-tech/library/Release/cc-tech.js";
 const assetStream = Testing(AssetStream);
-assetStream({ command: "Add" });
+assetStream({ command: "Add" }).claim((result) =>
+{
+	return isDefined(result.request);
+});
