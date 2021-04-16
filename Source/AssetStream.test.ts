e0bc356168623a98e60eff90b0433e42f23f9b64
Brietoe <Brietoe@gmail.com>
2021-04-16 18:19:42 -0500

    Fix library path

--- Source/AssetStream.test.ts
+++ Source/AssetStream.test.ts
@@ -1,6 +1,6 @@
 import AssetStream from "./AssetStream.js";
 import Testing from "../../Testing/Release/Testing.js";
-import { isDefined } from "../../Testing/node_modules/@cc-tech/library/Release/cc-tech.js";
+import { isDefined } from "@cc-tech/library";
 const assetStream = Testing(AssetStream);
 
 const testCommand = { command: "Add" };
