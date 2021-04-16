9655c11446db36e7ef4f721a693a52a0675ae1a6
Brietoe <Brietoe@gmail.com>
2021-04-16 18:20:49 -0500

    FINAL TEST for [CI automated build]

--- Source/AssetStream.test.ts
+++ Source/AssetStream.test.ts
@@ -4,6 +4,7 @@ import { isDefined } from "@cc-tech/library";
 const assetStream = Testing(AssetStream);
 
 const testCommand = { command: "Add" };
+
 assetStream(testCommand).claim((result) =>
 {
 	return isDefined(result.request);
