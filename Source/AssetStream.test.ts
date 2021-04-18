78eaaa73c75669b9c2673cd585330a70ece47125
Brietoe <Brietoe@gmail.com>
2021-04-17 19:48:20 -0500

    Installed modules via npm registry

--- Source/AssetStream.test.ts
+++ Source/AssetStream.test.ts
@@ -1,10 +1,12 @@
 import AssetStream from "./AssetStream.js";
 import { isDefined } from "@cc-tech/library";
+import Testing from "@cc-tech/testing";
+
 const assetStream = Testing(AssetStream);
 
 const testCommand = { command: "Add" };
 
-assetStream(testCommand).claim((result) =>
+assetStream(testCommand).claim((result: { request: any; }) =>
 {
 	return isDefined(result.request);
 });
